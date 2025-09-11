4a79428d017c9813298c63afcd0533f89197683a
John O'Farrell <johnofarrell12@gmail.com>
2025-09-11 14:45:51 -0400

    fixing deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/mooonchild/',
   publicDir: 'public',
   assetsInclude: ['**/*.gif', '**/*.wav', '**/*.mp3'],
   server: {
