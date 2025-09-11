c16921e3e052083786bb6f50c2b4233dd618351f
John O'Farrell <johnofarrell12@gmail.com>
2025-09-11 16:37:35 -0400

    changing moonchild update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mooonchild/',
+  base: '/moonchild/',
   publicDir: 'public',
   assetsInclude: ['**/*.gif', '**/*.wav', '**/*.mp3'],
   server: {
